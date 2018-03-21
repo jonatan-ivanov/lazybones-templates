365cfc3f7b69e36da00985943604e6b399b5a0a0
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2018-03-20 21:47:30 -0700

    Adding application.properties to nanoservice template

--- templates/nanoservice/lazybones.groovy
+++ templates/nanoservice/lazybones.groovy
@@ -1,6 +1,7 @@
 def props = [:]
 props.appName = ask("Define value for 'appName' [nanoservice]: ", 'nanoservice', 'appName')
 
+processTemplates 'config/application.properties', props
 processTemplates 'Dockerfile', props
 processTemplates 'docker-compose.yml', props
 processTemplates 'README.md', props
