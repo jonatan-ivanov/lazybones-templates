37f394f625c17623519aea201cd25dd2e90f68d3
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2018-04-09 13:29:20 -0700

    process .yml template instead of the old .properties

--- templates/nanoservice/lazybones.groovy
+++ templates/nanoservice/lazybones.groovy
@@ -1,7 +1,7 @@
 def props = [:]
 props.appName = ask("Define value for 'appName' [nanoservice]: ", 'nanoservice', 'appName')
 
-processTemplates 'config/application.properties', props
+processTemplates 'config/application.yml', props
 processTemplates 'Dockerfile', props
 processTemplates 'docker-compose.yml', props
 processTemplates 'README.md', props
